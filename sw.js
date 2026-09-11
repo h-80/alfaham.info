83d332eb9cd7ff893156e1b18df322ab94b871b0
Alfaham <alarajiiq@gmail.com>
2026-09-11 05:28:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'iraq-net-shell-v1';
+const CACHE_NAME = 'iraq-net-shell-v2';
 const APP_SHELL = ['./', './index.html', './article.html', './admin.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
